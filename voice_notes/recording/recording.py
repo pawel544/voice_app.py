9156e26b6df5657429d62a2604f197a4e7636b6c
pawel544 <140202153+pawel544@users.noreply.github.com>
2025-06-08 03:31:28 +0200

    Add files via upload
    Add recording timer with pause and resume functionality
    
    - Implemented real-time timer display during audio recording
    - Added support for pausing and resuming recording using threading events
    - Improved GUI responsiveness by limiting timer update frequency

--- voice_notes/recording/recording.py
+++ voice_notes/recording/recording.py
@@ -1,3 +1,4 @@
+
 from scipy.io.wavfile import write , read
 import sounddevice as sd
 import os
@@ -5,7 +6,7 @@ import soundfile as sf
 import PySimpleGUI  as sg
 import queue
 import numpy as np
-
+import time
 
 audio_queue = queue.Queue()
 sample_rate = 44100
@@ -13,16 +14,24 @@ def audio_callback(indata,frames, time, status):
 
     audio_queue.put(indata.copy())
 
-def start_recording( sample_rate, stop_event):
+def start_recording( sample_rate, stop_event,pause_event,window):
 
     try:
 
         with sd.InputStream(samplerate=sample_rate, channels= 1, callback= 	audio_callback):
             frames = []
-
+            last_update_time = 0
+            start = time.time()
             while not stop_event.is_set():
+                    if pause_event.is_set():
+
+                        pause_event.wait()
 
                     frames.append(audio_queue.get())
+                    elapse = (time.time() - start)
+                    if elapse - last_update_time > 0.2:
+                        window.write_event_value('-TIMER-', f"{elapse:.2f}")
+                        last_update_time = elapse
 
 
             if frames:
--- voice_notes/voice_app.py
+++ voice_notes/voice_app.py
@@ -1,4 +1,5 @@
 from recording.recording import start_recording, play_recording
+
 from speech.speech import recognize_speech
 import os
 import threading
@@ -7,30 +8,44 @@ import PySimpleGUI as sg
 if not os.path.exists("sound"):
     fol= os.mkdir("sound")
 
+
+pause_event = None
+stop_event = None
+thered = None
 sample_rate = 44100
 layout=[[sg.Text("Program")],
         [sg.Button("Nagrywaj"),sg.Button("Stop"), sg.Button("Odtwórz"), sg.Button("Notatka Głosowa"), sg.Button("EXIT")],
-        [sg.Text(key='-OUTPUT-')]]
+        [sg.Button("Zatszymaj"), sg.Button("Wznów")],
+        [sg.Text(key='-OUTPUT-')],
+        [sg.Text(key='-TIMER-')]]
 
 window = sg.Window(("Program"),layout, resizable= True)
 while True:
     try:
         event, value = window.read()
-        print(event, value)
-        if event == sg.WIN_CLOSED or event == "EXIT":
-            break
+
+        if event == sg.WIN_CLOSED or event == "EXIT": break
+
         elif event == "Nagrywaj":
+
+           pause_event = threading.Event()
            stop_event = threading.Event()
-           thered= threading.Thread(target= start_recording, args=( sample_rate,stop_event))
+           thered= threading.Thread(target= start_recording, args=( sample_rate,stop_event,pause_event,window))
            thered.start()
+
            window['-OUTPUT-'].update(f"Nagranie rospoczęte")
         elif event == "Stop":
 
             stop_event.set()
             thered.join()
-           # is_recording = False
-            #start_recording()
+
             window['-OUTPUT-'].update(f"Nagranie wstszymane")
+        elif event == '-TIMER-':
+            window['-TIMER-'].update(value)
+        elif event =="Zatszymaj":
+            pause_event.set()
+        elif event == "Wznów":
+            pause_event.clear()
         elif event == "Notatka Głosowa" :
             recognize_speech()
     except Exception as e:
